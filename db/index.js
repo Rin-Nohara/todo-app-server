c19ff0db2ebd4f2ff7b82b0691b87852a10d45e5
gaearon <lijq1103@163.com>
2022-04-09 18:01:08 +0800

    feat: 调整表结构
    直接使用 belongsTo 造成外键数据类型不匹配。
    调整为在设置表结构时，设置外键

--- db/index.js
+++ db/index.js
@@ -1,11 +1,7 @@
+const { DataTypes } = require("Sequelize");
 const seq = require('./db')
 const userModel = require('./user.model')
 const thingModel = require('./thing.model')
-
-// 外键关联
-thingModel.belongsTo(userModel, {
-  foreignKey: 'userName',
-})
 
 module.exports = {
   seq,
--- db/thing.model.js
+++ db/thing.model.js
@@ -1,5 +1,6 @@
 const { DataTypes } = require('sequelize')
 const seq = require('./db')
+const userModel = require('./user.model')
 
 const Thing = seq.define('thing', {
   thingName: {
@@ -9,6 +10,16 @@ const Thing = seq.define('thing', {
   thingDescription: {
     type: DataTypes.STRING,
     allowNull: true
+  },
+  // 可以创建外键：
+  userName: {
+    type: DataTypes.STRING,
+    references: {
+      // 这是对另一个模型的参考
+      model: userModel,
+      // 这是引用模型的列名
+      key: 'userName',
+    }
   },
 });
 
